d6de111343f6405c63e5ea5b71d71cb13e56fd16
Jakub <kontakt@milygosc.pl>
2020-02-07 11:31:03 +0100

    #1 - init commit with first technology stack

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Maillo
-Email marketing tool
+<h1 align="center">Maillo</h1>
+<h2 align="center">Email marketing tool</h2>
+
+## Project management
+Maillo development is planned and managed on [Trello](https://trello.com/b/wSlw10Cy/maillo).
---
+++ src/main/java/dev/maillo/MailloApplication.java
@@ -0,0 +1,13 @@
+package dev.maillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailloApplication.class, args);
+    }
+
+}
