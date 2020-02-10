58357ee02c8a065cd42b7848be4770ae147f6d2f
Jakub <kontakt@milygosc.pl>
2020-02-10 16:45:12 +0100

    refactor getters & setters to lombok @Data

--- src/main/java/dev/maillo/user/domain/User.java
+++ src/main/java/dev/maillo/user/domain/User.java
@@ -1,52 +1,15 @@
 package dev.maillo.user.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 import java.util.Set;
 
+@Data
 public class User {
     private String id;
     private String username;
     private String password;
     private Set<String> roles;
     private Date created;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
 }
