dd7ffd0edeaea7107aee4190fd0a6a7995d1476d
Jakub <kontakt@milygosc.pl>
2020-02-10 23:03:44 +0100

    setup basic security for login page

---
+++ src/main/java/dev/maillo/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package dev.maillo.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+        http.csrf().disable();
+    }
+}
