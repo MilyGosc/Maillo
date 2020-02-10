1886587e1fd50529215af1cb58df265a68a7e2ce
Jakub <kontakt@milygosc.pl>
2020-02-10 23:03:59 +0100

    #3 - create first revision of login template

---
+++ src/main/java/dev/maillo/auth/web/LoginController.java
@@ -0,0 +1,13 @@
+package dev.maillo.auth.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+}
