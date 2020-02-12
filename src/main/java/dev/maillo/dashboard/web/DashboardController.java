6ab22d313a4888f87d797a6cb5444a347863e9e0
Jakub <kontakt@milygosc.pl>
2020-02-12 23:03:41 +0100

    #5 - add basic topbar template

---
+++ src/main/java/dev/maillo/dashboard/web/DashboardController.java
@@ -0,0 +1,13 @@
+package dev.maillo.dashboard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard/dashboard";
+    }
+}
